f37bab3293aa3c3de8904864ed5aa75cdf1af9b6
Ashvin Bambhaniya <120353560+AshvinBambhaniya@users.noreply.github.com>
2024-03-12 23:19:57 +0530

    solve que 1 (#3)
    * solve que 1
    
    * Refactor code
    
    ---------
    
    Co-authored-by: Ashvin Bambhaniya <ashvin.bambhaniya@improwised.com>

---
+++ Que1/ashvin.go
@@ -0,0 +1,29 @@
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	prefix := strs[0]
+	for i := 1; i < len(strs); i++ {
+		for {
+			if len(prefix) > len(strs[i]) {
+				prefix = prefix[:len(prefix)-1]
+				if prefix == "" {
+					return ""
+				}
+			} else {
+				break
+			}
+		}
+		for {
+			if strs[i][:len(prefix)] == prefix {
+				break
+			} else {
+				prefix = prefix[:len(prefix)-1]
+				if prefix == "" {
+					return ""
+				}
+			}
+		}
+	}
+	return prefix
+}
