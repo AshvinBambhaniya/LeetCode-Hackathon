b766d2b932f1c8f974a8844ada848473dc667108
husen kureshi <husen.kureshi@improwised.com>
2024-03-15 10:05:21 +0530

    longest prefix question solved

--- Que1/husen.go
+++ Que1/husen.go
@@ -1,42 +1,33 @@
 func longestCommonPrefix(strs []string) string {
-    var answer string
-    var ch string
-    pos := -1
-    if len(strs) == 0{
-        return answer
-    }
-    if len(strs) <= 1{
-        return strs[0]
-    }
-    first := strs[0]
-    for i:=0; i<len(first); i++{
-    found := false
+	var answer string
 
-        ch = string(first[i]) 
+	if len(strs) == 0 {
+		return answer
+	}
+	if len(strs) <= 1 {
+		return strs[0]
+	}
+	first := strs[0]
 
-        for j := 1; j < len(strs); j++ {
+	for i, ch := range first {
+		found := true
+		for _, str := range strs[1:len(strs)] {
 
-            str := strs[j]
-            for k:=0; k < len(str); k++{
-                if string(str[k]) == ch {
-                    if pos < 0 {
-                        found = true
-                        pos++
-                        break
-                    }else if k > 0 && str[k-1] == answer[pos]{
-                        found = true
-                        pos++
-                     break
-                    }
-                } 
-                   
-            }
-                found = false
-            }
-        }
-        if found {
-                answer = fmt.Sprintf("%s%s",answer,ch)
-            }     
-    }
-    return answer
+			if len(str) <= i {
+				found = false
+				break
+			}
+			if byte(ch) != str[i] {
+				found = false
+				break
+			}
+
+		}
+		if !found {
+			break
+		} else {
+			answer = fmt.Sprintf("%s%s", string(answer), string(ch))
+		}
+	}
+	return answer
 }
